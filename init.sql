b60ec25fb6e2183814fd9e4765433d83773f7b01
tutrahamon <tutrachamon@mail.ru>
2025-06-10 16:50:05 +0300

    create analyzer-service

--- init.sql
+++ init.sql
@@ -2,14 +2,17 @@ CREATE DATABASE comment_db;
 CREATE DATABASE event_db;
 CREATE DATABASE request_db;
 CREATE DATABASE user_db;
+CREATE DATABASE analyzer_db;
 
 
 CREATE USER comment_user WITH PASSWORD 'password1';
 CREATE USER event_user WITH PASSWORD 'password2';
 CREATE USER request_user WITH PASSWORD 'password3';
 CREATE USER user_user WITH PASSWORD 'password4';
+CREATE USER analyzer_user WITH PASSWORD 'password5';
 
 ALTER DATABASE comment_db OWNER TO comment_user;
 ALTER DATABASE event_db OWNER TO event_user;
 ALTER DATABASE request_db OWNER TO request_user;
 ALTER DATABASE user_db OWNER TO user_user;
+ALTER DATABASE analyzer_db OWNER TO analyzer_user;
